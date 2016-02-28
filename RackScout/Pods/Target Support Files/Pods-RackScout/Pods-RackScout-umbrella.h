b7bfefeabf3d40f548f81f41f0cdf50d2b3f655f
Kevin Li <kevinli.kvn120@yahoo.com>
2016-02-28 14:52:14 -0800

    Added Firebase framework through Cocoapods

---
+++ RackScout/Pods/Target Support Files/Pods-RackScout/Pods-RackScout-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_RackScoutVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_RackScoutVersionString[];
+
---
+++ RackScout/Pods/Target Support Files/Pods-RackScoutUITests/Pods-RackScoutUITests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_RackScoutUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_RackScoutUITestsVersionString[];
+
