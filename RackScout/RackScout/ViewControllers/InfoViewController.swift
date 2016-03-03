0f38cd98fe4f298b44b4a05917beab116a6ff4ae
Kevin Li <kevinli120@yahoo.com>
2016-03-02 18:33:43 -0800

    added SMCalloutView

---
+++ RackScout/RackScout/ViewControllers/InfoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InfoViewController.swift
+//  RackScout
+//
+//  Created by Kevin Li on 3/2/16.
+//  Copyright © 2016 Kevin Li. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+
+}
--- RackScout/RackScout/ViewControllers/MapViewController.swift
+++ RackScout/RackScout/ViewControllers/MapViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 import Firebase
 
-class MapViewController: UIViewController, CLLocationManagerDelegate {
+import SMCalloutView
+
+class MapViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDelegate {
     
     let locationManager = CLLocationManager()
+    
+    var calloutView = SMCalloutView()
+    var emptyCalloutView = UIView(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+    let CalloutYOffset: CGFloat = 40.0
     
     @IBOutlet weak var mapView: GMSMapView!
     
@@ -22,9 +28,9 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        setUpLocationManager();
-        
-        test();
+        setUpLocationManager()
+        setUpCalloutView()
+        setUpMap()
         
     }
     
@@ -36,7 +42,6 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
     // MARK: Location
     
     func setUpLocationManager() {
-        
         locationManager.requestWhenInUseAuthorization()
         
         if CLLocationManager.locationServicesEnabled() {
@@ -47,42 +52,91 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        mapView.myLocationEnabled = true
-        
+        // Change camera view only once
         if let location = locationManager.location {
             mapView.camera = GMSCameraPosition.cameraWithLatitude(location.coordinate.latitude, longitude: location.coordinate.longitude, zoom: 16)
         }
+        
+        locationManager.stopUpdatingLocation()
     }
     
-    // MARK: Testing -- DELETE
+    // MARK: Map Setup
     
-    func test() {
+    func setUpMap() {
+        mapView.delegate = self
+        mapView.myLocationEnabled = true
+        mapView.settings.myLocationButton = true
+        mapView.padding = UIEdgeInsets(top: 64.0, left: 0.0, bottom: 60.0, right: 0.0)
         
-//        let camera = GMSCameraPosition.cameraWithLatitude(-33.86,
-//            longitude: 151.20, zoom: 6)
-//        let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
-//        mapView.myLocationEnabled = true
-//        self.view = mapView
-//        
-//        let marker = GMSMarker()
-//        marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
-//        marker.title = "Sydney"
-//        marker.snippet = "Australia"
-//        marker.map = mapView
-        
-//        let ref = Firebase(url: "https://rackscout.firebaseio.com/")
-//        
-//        let bikeRacksRef = ref.childByAppendingPath("bikeRacks")
-//        
-//        var testBikeRack = [
-//            "id": "0",
-//            "testProperty": "Blarg"
-//        ]
-//        
-//        var bikeRacks = ["0": testBikeRack]
-//        
-//        bikeRacksRef.setValue(bikeRacks)
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2DMake(37.331694, -122.030219)
+        marker.title = "Apple"
+        marker.snippet = "Headquarters"
+        marker.map = mapView
     }
     
+    func mapView(mapView: GMSMapView, markerInfoWindow marker: GMSMarker) -> UIView? {
+        
+        // Callout View Setup
+
+        let anchor = marker.position
+        
+        let point = mapView.projection.pointForCoordinate(anchor)
+        
+        calloutView.title = marker.title
+        calloutView.calloutOffset = CGPoint(x: 0, y: -CalloutYOffset)
+        calloutView.hidden = false
+        
+        var calloutRect = CGRectZero
+        calloutRect.origin = point
+        calloutRect.size = CGSizeZero
+        
+        calloutView.presentCalloutFromRect(calloutRect, inView: mapView, constrainedToView: mapView, animated: true)
+        
+        return self.emptyCalloutView
+    }
+    
+    func mapView(mapView: GMSMapView, didChangeCameraPosition position: GMSCameraPosition) {
+        
+        // Callout View Setup
+        
+        if mapView.selectedMarker != nil && !calloutView.hidden {
+            let anchor = mapView.selectedMarker!.position
+            let arrowPt = calloutView.backgroundView.arrowPoint
+            var pt = mapView.projection.pointForCoordinate(anchor)
+            
+            pt.x -= arrowPt.x
+            pt.y -= arrowPt.y + CalloutYOffset
+            
+            calloutView.frame = CGRect(origin: pt, size: calloutView.frame.size)
+        } else {
+            calloutView.hidden = true
+        }
+    }
+    
+    func mapView(mapView: GMSMapView, didTapAtCoordinate coordinate: CLLocationCoordinate2D) {
+        // Callout View Setup
+        calloutView.hidden = true
+    }
+    
+    func mapView(mapView: GMSMapView, didTapMarker marker: GMSMarker) -> Bool {
+        // Callout View Setup
+        mapView.selectedMarker = marker
+        return true
+    }
+    
+    // MARK: Setup
+
+    func setUpCalloutView() {
+        let button = UIButton(type: UIButtonType.DetailDisclosure)
+        button.addTarget(self, action: Selector("calloutButtonTapped"), forControlEvents: UIControlEvents.TouchUpInside)
+        
+        calloutView.rightAccessoryView = button as UIView
+    }
+    
+    // MARK: Navigation
+    func calloutButtonTapped() {
+        self.performSegueWithIdentifier("showBikeRackInfo", sender: nil)
+    }
     
 }
